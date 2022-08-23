56b5a47c85fbb2aca076fea432d25e93fa943302
Jon <jon.bloomer@iag.com.au>
2022-08-23 15:59:43 +1000

    updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   integrations: [react()],
 });
