2a7480ded2750de23f60d87f9eff9ff97d8bc366
Jon <jon.bloomer@iag.com.au>
2023-06-01 08:36:06 +1000

    updated serif font to Bodoni Moda

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,28 @@
-/** @type {import('tailwindcss').Config} */ 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,astro}"],
+  content: ['./src/**/*.{html,js,jsx,astro}'],
   theme: {
     fontFamily: {
       sans: ['Arial', 'Helvetica', 'sans-serif'],
       cursive: ['Sacramento', 'cursive'],
-      serif: ['Zilla Slab', 'serif'],
+      serif: ['Bodoni Moda', 'serif'],
     },
     screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1140px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1140px',
     },
     extend: {
       colors: {
-        'beige': '#e2dfdb'
+        beige: '#e2dfdb',
       },
       keyframes: {
         fadeIn: {
           '0%': { opacity: '0' },
-          '100%': { opacity: '1' }
-        }
-      }
+          '100%': { opacity: '1' },
+        },
+      },
     },
   },
   plugins: [],
